5327547635bc99250c3c2756c2a0a536c0795adb
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2003-10-29 16:58:43 +0000

    Added "add" function.  Arrays and single file names are acceptable.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@52 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake/filelist.rb
+++ rake/lib/rake/filelist.rb
@@ -6,8 +6,21 @@ module Rake
       add_matching(pattern) if pattern
     end
 
-    def add_matching(pattern)
-      Dir[pattern].each { |fn| self << fn } if pattern
+    def add(*filenames)
+      filenames.each do |fn|
+	case fn
+	when Array
+	  fn.each { |f| self << f }
+	else
+	  self << fn
+	end
+      end
+    end
+
+    def add_matching(*patterns)
+      patterns.each do |pattern|
+	Dir[pattern].each { |fn| self << fn } if pattern
+      end
     end
 
     def to_s
