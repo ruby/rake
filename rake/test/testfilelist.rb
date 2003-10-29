4f051698070a5c38f3dac19cffec29884f8fc515
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2003-10-29 16:58:45 +0000

    New tests for added FileList functionality. Added tests for multiple patterns, reject! and uniq!.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@53 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/test/testfilelist.rb
+++ rake/test/testfilelist.rb
@@ -7,20 +7,28 @@ class TestFileList < Test::Unit::TestCase
   def test_create
     fl = Rake::FileList.new
     assert_not_nil fl
+    assert_equal 0, fl.size
+  end
+
+  def test_add
+    fl = Rake::FileList.new
+    fl << "a.rb" << "b.rb"
+    assert_equal ['a.rb', 'b.rb'], fl
+  end
+
+  def test_add_many
+    fl = Rake::FileList.new
+    fl.add %w(a d c )
+    fl.add('x', 'y')
+    assert_equal ['a', 'd', 'c', 'x', 'y'], fl
   end
 
   def test_match
-    fl = Rake::FileList.new('test/*.rb')
+    fl = Rake::FileList.new
+    fl.add_matching('test/test*.rb')
     assert fl.include?("test/testfilelist.rb")
     assert fl.size > 3
     fl.each { |fn| assert_match /\.rb$/, fn }
-  end
-
-  def test_to_string
-    fl = Rake::FileList.new
-    fl << "a.java" << "b.java"
-    assert_equal  "a.java b.java", fl.to_s
-    assert_equal  "a.java b.java", "#{fl}"
   end
 
   def test_add_matching
@@ -31,4 +39,39 @@ class TestFileList < Test::Unit::TestCase
     assert fl.size > 2
     assert fl.include?("test/testfilelist.rb")
   end
+
+  def test_multiple_patterns
+    fl = Rake::FileList.new
+    fl.add_matching('*.c', '*xist*')
+    assert_equal [], fl
+    fl.add_matching('testdata/*.c', 'testdata/*xist*')
+    assert_equal [
+      'testdata/x.c', 'testdata/xyz.c', 'testdata/abc.c', 'testdata/existing'
+    ].sort, fl.sort
+  end
+
+  def test_reject
+    fl = Rake::FileList.new
+    fl.add %w(testdata/x.c testdata/abc.c testdata/xyz.c testdata/existing)
+    fl.reject! { |fn| fn =~ %r{/x} }
+    assert_equal [
+      'testdata/abc.c', 'testdata/existing'
+    ], fl
+  end
+
+  def test_unique
+    fl = Rake::FileList.new
+    fl << "x.c" << "a.c" << "b.rb" << "a.c"
+    assert_equal ['x.c', 'a.c', 'b.rb', 'a.c'], fl
+    fl.uniq!
+    assert_equal ['x.c', 'a.c', 'b.rb'], fl
+  end
+
+  def test_to_string
+    fl = Rake::FileList.new
+    fl << "a.java" << "b.java"
+    assert_equal  "a.java b.java", fl.to_s
+    assert_equal  "a.java b.java", "#{fl}"
+  end
+
 end
