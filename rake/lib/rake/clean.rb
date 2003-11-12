bc20738288a0f49eddb4b414ecb05d61e21aa493
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2003-11-12 05:26:29 +0000

    Added comments to the clean/clobber tasks.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@90 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake/clean.rb
+++ rake/lib/rake/clean.rb
@@ -6,12 +6,14 @@ require 'rake/filelist'
 CLEAN = Rake::FileList.new
 CLEAN.add("**/*~", "**/*.bak", "**/core")
 
+desc "Remove any temporary products."
 task :clean do
   CLEAN.each { |fn| rm_r fn rescue nil }
 end
 
 CLOBBER = Rake::FileList.new
 
+desc "Remove any generated file."
 task :clobber => [:clean] do
   CLOBBER.each { |fn| rm_r fn rescue nil }
 end
