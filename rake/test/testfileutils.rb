dc7eaab3cdefde7c8ba1d4c24b48a403edf5610d
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2003-11-12 04:38:10 +0000

    Removed old tests (were commented out). Added tests for verbose/nowrite.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@87 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/test/testfileutils.rb
+++ rake/test/testfileutils.rb
@@ -29,35 +29,26 @@ class TestFileUtils < Test::Unit::TestCase
     assert ! File.exist?("testdata/b")
   end
 
-#   def test_delete
-#     create_file("testdata/a")
-#     Sys.delete_all("testdata/a")
-#     assert ! File.exist?("testdata/a")
-#   end
-
-#   def test_copy
-#     create_file("testdata/a")
-#     Sys.copy("testdata/a", "testdata/b")
-#     assert File.exist?("testdata/b")
-#   end
-
-#   def test_for_files
-#     test_files = ["testdata/a.pl", "testdata/c.pl", "testdata/b.rb"]
-#     test_files.each { |fn| create_file(fn) }
-#     list = []
-#     Sys.for_files("testdata/*.pl", "testdata/*.rb") { |fn|
-#       list << fn
-#     }
-#     assert_equal test_files.sort, list.sort
-#   end
-
-#   def test_indir
-#     here = Dir.pwd
-#     Sys.makedirs("testdata/dir")
-#     assert_equal "#{here}/testdata/dir", Sys.indir("testdata/dir") { Dir.pwd }
-#     assert_equal here, Dir.pwd
-#   end
-
-  def test_nothing
+  def test_verbose
+    verbose true
+    assert_equal true, verbose
+    verbose false
+    assert_equal false, verbose
+    verbose(true){
+      assert_equal true, verbose
+    }
+    assert_equal false, verbose
   end
+
+  def test_nowrite
+    nowrite true
+    assert_equal true, nowrite
+    nowrite false
+    assert_equal false, nowrite
+    nowrite(true){
+      assert_equal true, nowrite
+    }
+    assert_equal false, nowrite
+  end
+
 end
