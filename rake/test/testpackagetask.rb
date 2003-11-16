14d757dde1896cc2fb1ea2ed7fe909bfc5b8ba41
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2003-11-16 22:18:50 +0000

    package now built in constructor.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@100 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/test/testpackagetask.rb
+++ rake/test/testpackagetask.rb
@@ -5,17 +5,21 @@ require 'rake/packagetask'
 
 class TestPackageTask < Test::Unit::TestCase
   def test_create
-    pkg = Rake::PackageTask.new("pkgr", "1.2.3")
-    pkg.package_files << "install.rb"
-    pkg.package_files.add(
-      '[A-Z]*',
-      'bin/**/*',
-      'lib/**/*.rb',
-      'test/**/*.rb',
-      'doc/**/*',
-      'build/rubyapp.rb',
-      '*.blurb')
-    pkg.define
+    pkg = Rake::PackageTask.new("pkgr", "1.2.3") { |p|
+      p.package_files << "install.rb"
+      p.package_files.add(
+	'[A-Z]*',
+	'bin/**/*',
+	'lib/**/*.rb',
+	'test/**/*.rb',
+	'doc/**/*',
+	'build/rubyapp.rb',
+	'*.blurb')
+      p.package_files.exclude(/\bCVS\b/)
+      p.package_files.exclude(/~$/)
+      p.need_tar = true
+      p.need_zip = true
+    }
     assert_equal "pkg", pkg.package_dir
     assert pkg.package_files.include?("bin/rake")
     assert "pkgr", pkg.name
