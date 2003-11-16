fd2693b4a23778842a9c56295ca64f327801e36c
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2003-11-16 22:18:44 +0000

    added gemspec to package task library.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@97 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake/packagetask.rb
+++ rake/lib/rake/packagetask.rb
@@ -4,23 +4,30 @@ module Rake
 
   class PackageTask
     attr_accessor :name, :version, :package_dir
+    attr_accessor :gem_spec
     attr_accessor :need_tar, :need_zip
     attr_reader :package_files
 
-    def initialize(name, version)
+    def initialize(name=nil, version=nil)
       @name = name
       @version = version
       @package_files = Rake::FileList.new
       @package_dir = 'pkg'
-      @need_tar = true
-      @need_zip = true
+      @need_tar = false
+      @need_zip = false
+      @gem_spec = nil
       yield self if block_given?
       define
     end
 
     def define
+      copy_from_gem if gem_spec
+
       desc "Build the package"
       task :package
+      
+      desc "Create a RubyGem for #{name}"
+      task :gem
 
       desc "Force a rebuild of the package files"
       task :repackage => [:clobber_package, :package]
@@ -31,10 +38,10 @@ module Rake
       end
 
       task :clobber => [:clobber_package]
-      
+
       if need_tar
 	task :package => ["#{package_dir}/#{tgz_file}"]
-	file "#{package_dir}/#{tgz_file}" => [package_dir_path] do
+	file "#{package_dir}/#{tgz_file}" => [package_dir_path] + package_files do
 	  chdir(package_dir) do
 	    sh %{tar zcvf #{tgz_file} #{package_name}}
 	  end
@@ -43,12 +50,27 @@ module Rake
 
       if need_zip
 	task :package => ["#{package_dir}/#{zip_file}"]
-	file "#{package_dir}/#{zip_file}" => [package_dir_path] do
+	file "#{package_dir}/#{zip_file}" => [package_dir_path] + package_files do
 	  chdir(package_dir) do
 	    sh %{zip -r #{zip_file} #{package_name}}
 	  end
 	end
       end
+
+      if gem_spec
+	task :package => [:gem]
+	task :gem => ["#{package_dir}/#{gem_file}"]
+	file "#{package_dir}/#{gem_file}" => [package_dir] + package_files do
+	  when_writing("Creating GEM") {
+	    Gem::Builder.new(gem_spec).build
+	    verbose(false) {
+	      mv gem_file, "#{package_dir}/#{gem_file}"
+	    }
+	  }
+	end
+      end
+
+      directory package_dir
 
       file package_dir_path => @package_files do
 	mkdir_p package_dir rescue nil
@@ -59,6 +81,7 @@ module Rake
 	  if File.directory?(fn)
 	    mkdir_p(f)
 	  else
+	    rm_f f
 	    ln(fn, f)
 	  end
 	end
@@ -68,12 +91,22 @@ module Rake
 
     private
 
+    def copy_from_gem
+      @name = gem_spec.name
+      @version = gem_spec.version
+      @package_files += gem_spec.files if gem_spec.files
+    end
+
     def package_name
       "#{@name}-#{@version}"
     end
       
     def package_dir_path
       "#{package_dir}/#{package_name}"
+    end
+
+    def gem_file
+      "#{package_name}.gem"
     end
 
     def tgz_file
