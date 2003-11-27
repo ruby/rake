1ed436ceebc0b77b3991ebdc089459dd91b59341
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2003-11-27 13:59:51 +0000

    RDocTask now clonable. No default for main. Title option added. Fixed rerdoc to use clobber instead of clean. Options are now handled properly.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@111 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake/rdoctask.rb
+++ rake/lib/rake/rdoctask.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+require 'rake'
+require 'rake/tasklib'
+
 module Rake
 
   # Create a documentation task that will generate the RDoc files for
@@ -9,9 +12,6 @@ module Rake
   #
   # [<b><em>name</em></b>]
   #   Main task for this RDOC task.  
-  #
-  # [<b>:rdoc</b>]
-  #   Create the RDoc HTML files.  If task is only recreated 
   #
   # [<b>:clobber_<em>name</em></b>]
   #   Delete all the package files.  This target is automatically
@@ -25,54 +25,56 @@ module Rake
   #
   #   RDocTask.new do |rd|
   #     rd.main = "README.rdoc"
-  #     rd.package_files.add("lib/**/*.rb")
+  #     rd.package_files.include("lib/**/*.rb")
   #   end
   #
-  class RDocTask
+  class RDocTask < TaskLib
     # Name of the main, top level task.  (default is :rdoc)
     attr_accessor :name
 
     # Name of directory to receive the html output files. (default is "html")
     attr_accessor :rdoc_dir
 
+    # Title of RDoc documentation. (default is none)
+    attr_accessor :title
+
     # Name of file to be used as the main, top level file of the
-    # RDoc. (default 'README')
+    # RDoc. (default is none)
     attr_accessor :main
 
     # Name of template to be used by rdoc. (default is 'html')
     attr_accessor :template
 
     # List of files to be included in the rdoc generation. (default is [])
-    attr_reader :rdoc_files
+    attr_accessor :rdoc_files
 
     # List of options to be passed rdoc.  (default is [])
-    attr_reader :options
+    attr_accessor :options
 
     # Create an RDoc task named <em>name</em>.  
     def initialize(name=:rdoc)
       @name = name
       @rdoc_files = Rake::FileList.new
       @rdoc_dir = 'html'
-      @main = 'README'
+      @main = nil
+      @title = nil
       @template = 'html'
       @options = []
       yield self if block_given?
       define
     end
     
-    private
-
+    # Create the tasks defined by this task lib.
     def define
       if name.to_s != "rdoc"
 	desc "Build the RDOC HTML Files"
-	task :rdoc => [name]
       end
 
       desc "Build the #{name} HTML Files"
       task name
       
       desc "Force a rebuild of the RDOC files"
-      task paste("re", name) => [paste("clean_", name), name]
+      task paste("re", name) => [paste("clobber_", name), name]
       
       desc "Remove rdoc products" 
       task paste("clobber_", name) do
@@ -85,11 +87,17 @@ module Rake
       task name => [rdoc_target]
       file rdoc_target => @rdoc_files + ["Rakefile"] do
 	rm_r @rdoc_dir rescue nil
-	sh %{rdoc -o #{@rdoc_dir} #{@options} --main #{main} -T #{template} #{@rdoc_files}}
+	opts = @options.join(' ')
+	opts << " --main '#{main}'" if main
+	opts << " --title '#{title}'" if title
+	opts << " -T '#{template}'" if template
+	sh %{rdoc -o #{@rdoc_dir} #{opts} #{@rdoc_files}}
       end
       self
     end
     
+    private
+
     def rdoc_target
       "#{rdoc_dir}/index.html"
     end
