de34881e6fe0525cfc132e42d0afdc2e1292bbf6
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2003-11-28 04:15:04 +0000

    Added paste.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@129 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake/tasklib.rb
+++ rake/lib/rake/tasklib.rb
@@ -2,7 +2,10 @@
 
 module Rake
 
+  # Base class for Task Libraries.
   class TaskLib
+
+    # Make a copy of a task.
     def clone
       sibling = self.class.new
       instance_variables.each do |ivar|
@@ -10,6 +13,11 @@ module Rake
 	sibling.instance_variable_set(ivar, value.dup) if value
       end
       sibling
+    end
+
+    # Make a symbol by pasting two strings together. 
+    def paste(a,b)
+      (a.to_s + b.to_s).intern
     end
   end
 
