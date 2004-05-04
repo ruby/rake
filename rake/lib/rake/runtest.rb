a2b4e954b34a7b389be97f2160db04c19e4ee08f
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2004-05-04 02:50:13 +0000

    Added assertions file to handle errors.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@181 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake/runtest.rb
+++ rake/lib/rake/runtest.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
+require 'test/unit/assertions'
 
 module Rake
+  include Test::Unit::Assertions
+
   def run_tests(pattern='test/test*.rb', log_enabled=false)
     Dir["#{pattern}"].each { |fn|
       puts fn if log_enabled
