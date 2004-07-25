e8c0015ece65eebb305df6280b8bc9a5f6a899af
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2004-07-25 12:27:00 +0000

    upgraded comments in testtask
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@249 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake/testtask.rb
+++ rake/lib/rake/testtask.rb
@@ -13,9 +13,26 @@ module Rake
   #  
   #   Rake::TestTask.new do |t|
   #     t.libs << "test"
-  #     t.pattern = '*test.rb'
+  #     t.test_files = FileList['test/test*.rb']
   #     t.verbose = true
   #   end
+  #
+  # If rake is invoked with a "TEST=filename" command line option,
+  # then the list of test files will be overridden to include only the
+  # filename specified on the command line.  This provides an easy way
+  # to run just one test.
+  #
+  # If rake is invoked with a "TESTOPTS=options" command line option,
+  # then the given options are passed to the test process after a
+  # '--'.  This allows Test::Unit options to be passed to the test
+  # suite.
+  #
+  # Examples:
+  #
+  #   rake test                           # run tests normally
+  #   rake test TEST=just_one_file.rb     # run just one test file.
+  #   rake test TESTOPTS="-v"             # run in verbose mode
+  #   rake test TESTOPTS="--runner=fox"   # use the fox test runner
   #
   class TestTask < TaskLib
 
