a77debd82d5c860d2b8ce057900c223902883aaf
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2004-08-26 11:31:40 +0000

    final changes for 0.4.6
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@260 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake.rb
+++ rake/lib/rake.rb
@@ -386,23 +386,21 @@ module FileUtils
     sh "#{RUBY} #{args.join(' ')}", options
   end
   
+  LN_SUPPORTED = [true]
+
   #  Attempt to do a normal file link, but fall back to a copy if the
   #  link fails.
   def safe_ln(*args)
-    if @link_not_supported
+    unless LN_SUPPORTED[0]
       cp(*args)
     else
       begin
 	ln(*args)
       rescue Errno::EOPNOTSUPP
-	@link_not_supported = true
+	LN_SUPPORTED[0] = false
 	cp(*args)
       end
     end
-  end
-
-  def ln(*args)
-    fail Errno::EOPNOTSUPP, "TESTING"
   end
 
   # Split a file path into individual directory names.
