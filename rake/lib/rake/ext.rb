a4b6d3642260024e560dc37ae1f84b20d782c97e
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2005-04-02 09:08:13 +0000

    updated comments in est file.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@377 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake/ext.rb
+++ rake/lib/rake/ext.rb
@@ -2,6 +2,12 @@
 
 class String
   unless instance_methods.include? "ext"
+    # Replace the file extension with +newext+.  If there is no
+    # extenson on the string, append the new extension to the end.  If
+    # the new extension is not given, or is the empty string, remove
+    # any existing extension.
+    #
+    # +ext+ is a user added method for the String class.
     def ext(newext='')
       return self.dup if ['.', '..'].include? self
       if newext != ''
@@ -14,6 +20,10 @@ end
 
 class Array
   unless instance_methods.include? "ext"
+    # Return a new array with <tt>String#ext</tt> method applied to
+    # each member of the array.
+    #
+    # +ext+ is a user added method for the Array class.
     def ext(newext='')
       collect { |fn| fn.ext(newext) }
     end
