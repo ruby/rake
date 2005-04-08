ab29a59d5b5354c14b98755478acd8c671938764
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2005-04-08 00:49:07 +0000

    Added check of tasks as well as existing files to rule checks.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@395 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake.rb
+++ rake/lib/rake.rb
@@ -303,7 +303,7 @@ class Task
           else
             fail "Don't know how to handle rule dependent: #{ext.inspect}"
           end
-          if File.exist?(source)
+          if File.exist?(source) || TASKS.has_key?(source)
             task = FileTask.define_task({task_name => [source]}, &block)
             task.source = source
             return task
