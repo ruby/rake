1f6e40b6fefcbe4242809401bf3d9adbf32d7f37
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2005-07-27 04:57:31 +0000

    enabled signed gem file for rake
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@440 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake.rb
+++ rake/lib/rake.rb
@@ -29,7 +29,7 @@
 # referenced as a library via a require statement, but it can be
 # distributed independently as an application.
 
-RAKEVERSION = '0.5.4.3'
+RAKEVERSION = '0.5.4.4'
 
 require 'rbconfig'
 require 'ftools'
