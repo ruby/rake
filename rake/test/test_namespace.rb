2e9e9a437dc87d285924a9c75f5fe46ba628b665
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2006-01-18 14:46:00 +0000

    added explicit namespace tests
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@518 5af023f1-ac1a-0410-98d6-829a145c37ef

---
+++ rake/test/test_namespace.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'test/unit'
+require 'flexmock'
+require 'rake'
+
+class TestNameSpace < Test::Unit::TestCase
+  def test_namespace_creation
+    FlexMock.use("TaskManager") do |mgr|
+      ns = Rake::NameSpace.new(mgr, [])
+      assert_not_nil ns
+    end
+  end
+
+  def test_namespace_lookup
+    FlexMock.use("TaskManager") do |mgr|
+      mgr.should_receive(:lookup).with(:t, ["a"]).and_return(nil)
+      ns = Rake::NameSpace.new(mgr, ["a"])
+      ns[:t]
+    end
+  end
+
+end
