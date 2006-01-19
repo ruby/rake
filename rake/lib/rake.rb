289be7bb4cfe1c8f9eff16d8d2af3ee4ed5cbf2d
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2006-01-19 12:38:19 +0000

    Updated to version 0.7.0
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@530 5af023f1-ac1a-0410-98d6-829a145c37ef

--- rake/lib/rake.rb
+++ rake/lib/rake.rb
@@ -29,7 +29,7 @@
 # referenced as a library via a require statement, but it can be
 # distributed independently as an application.
 
-RAKEVERSION = '0.6.99.3'
+RAKEVERSION = '0.7.0'
 
 require 'rbconfig'
 require 'ftools'
