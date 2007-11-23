354fbfa4494aca348df98360506839e12eeb898f
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2007-11-23 17:31:52 +0000

    removed debugging messages from functional tests
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@618 5af023f1-ac1a-0410-98d6-829a145c37ef

--- test/functional.rb
+++ test/functional.rb
@@ -4,14 +4,12 @@ begin
   require 'rubygems'
   gem 'session'
   require 'session'
-  puts 'OK!'
 rescue LoadError
   puts "UNABLE TO RUN FUNCTIONAL TESTS"
   puts "No Session Found"
 end
 
 if defined?(Session)
-  puts "RUNNING SESSIONS"
-  puts $:
+  puts "RUNNING WITH SESSIONS"
   require 'test/session_functional'
 end
