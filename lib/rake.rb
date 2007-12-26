663f1a1aba473e94b2feb36325ba8febb6598388
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2007-12-26 16:53:21 +0000

    Updated to version 0.8.1
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@630 5af023f1-ac1a-0410-98d6-829a145c37ef

--- lib/rake.rb
+++ lib/rake.rb
@@ -29,7 +29,7 @@
 # as a library via a require statement, but it can be distributed
 # independently as an application.
 
-RAKEVERSION = '0.8.0.99'
+RAKEVERSION = '0.8.1'
 
 require 'rbconfig'
 require 'getoptlong'
