2735ec963937f74d087827a5279f853f38bde8a6
Jim Weirich <jim.weirich@gmail.com>
2008-08-07 00:20:55 -0400

    bumped to version 0.8.1.5

--- lib/rake.rb
+++ lib/rake.rb
@@ -29,7 +29,7 @@
 # as a library via a require statement, but it can be distributed
 # independently as an application.
 
-RAKEVERSION = '0.8.1.4'
+RAKEVERSION = '0.8.1.5'
 
 require 'rbconfig'
 require 'getoptlong'
