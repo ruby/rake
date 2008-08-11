c85656eb2db2e05d56d83af30f54f4a3d1703321
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2008-08-11 15:50:46 +0000

    Updated dup and clone to have preper semantics w.r.t. taint and freeze.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@660 5af023f1-ac1a-0410-98d6-829a145c37ef

--- lib/rake.rb
+++ lib/rake.rb
@@ -267,16 +267,22 @@ module Rake
   module Cloneable
     # Clone an object by making a new object and setting all the instance
     # variables to the same values.
-    def clone
+    def dup
       sibling = self.class.new
       instance_variables.each do |ivar|
         value = self.instance_variable_get(ivar)
         new_value = value.clone rescue value
         sibling.instance_variable_set(ivar, new_value)
       end
+      sibling.taint if tainted?
       sibling
     end
-    alias dup clone
+
+    def clone
+      sibling = dup
+      sibling.freeze if frozen?
+      sibling
+    end
   end
 
   ####################################################################
