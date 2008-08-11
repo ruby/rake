b9228853385de4167ba899810f74ff9118de51b7
jimweirich <jimweirich@5af023f1-ac1a-0410-98d6-829a145c37ef>
2008-08-11 15:50:48 +0000

    cleaned up test_tasks
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rake/trunk@661 5af023f1-ac1a-0410-98d6-829a145c37ef

--- test/test_tasks.rb
+++ test/test_tasks.rb
@@ -6,20 +6,10 @@ require 'rake'
 require 'test/filecreation'
 require 'test/capture_stdout'
 
-
-module Interning
-  private
-
-  def intern(name, *args)
-    Rake.application.define_task(Rake::Task, name, *args)
-  end
-end
-
 ######################################################################
 class TestTask < Test::Unit::TestCase
   include CaptureStdout
   include Rake
-  include Interning
 
   def setup
     Task.clear
@@ -27,7 +17,7 @@ class TestTask < Test::Unit::TestCase
 
   def test_create
     arg = nil
-    t = intern(:name).enhance { |task| arg = task; 1234 }
+    t = task(:name) { |task| arg = task; 1234 }
     assert_equal "name", t.name
     assert_equal [], t.prerequisites
     assert t.prerequisites.is_a?(FileList)
@@ -39,26 +29,26 @@ class TestTask < Test::Unit::TestCase
   end
 
   def test_inspect
-    t = intern(:foo).enhance([:bar, :baz])
+    t = task(:foo, :needs => [:bar, :baz])
     assert_equal "<Rake::Task foo => [bar, baz]>", t.inspect
   end
 
   def test_invoke
     runlist = []
-    t1 = intern(:t1).enhance([:t2, :t3]) { |t| runlist << t.name; 3321 }
-    t2 = intern(:t2).enhance { |t| runlist << t.name }
-    t3 = intern(:t3).enhance { |t| runlist << t.name }
-    assert_equal [:t2, :t3], t1.prerequisites
+    t1 = task(:t1 => [:t2, :t3]) { |t| runlist << t.name; 3321 }
+    t2 = task(:t2) { |t| runlist << t.name }
+    t3 = task(:t3) { |t| runlist << t.name }
+    assert_equal ["t2", "t3"], t1.prerequisites
     t1.invoke
     assert_equal ["t2", "t3", "t1"], runlist
   end
 
   def test_invoke_with_circular_dependencies
     runlist = []
-    t1 = intern(:t1).enhance([:t2]) { |t| runlist << t.name; 3321 }
-    t2 = intern(:t2).enhance([:t1]) { |t| runlist << t.name }
-    assert_equal [:t2], t1.prerequisites
-    assert_equal [:t1], t2.prerequisites
+    t1 = task(:t1 => [:t2]) { |t| runlist << t.name; 3321 }
+    t2 = task(:t2 => [:t1]) { |t| runlist << t.name }
+    assert_equal ["t2"], t1.prerequisites
+    assert_equal ["t1"], t2.prerequisites
     ex = assert_raise RuntimeError do
       t1.invoke
     end
@@ -69,7 +59,7 @@ class TestTask < Test::Unit::TestCase
   def test_dry_run_prevents_actions
     Rake.application.options.dryrun = true
     runlist = []
-    t1 = intern(:t1).enhance { |t| runlist << t.name; 3321 }
+    t1 = task(:t1) { |t| runlist << t.name; 3321 }
     out = capture_stdout { t1.invoke }
     assert_match(/execute .*t1/i, out)
     assert_match(/dry run/i, out)
@@ -81,7 +71,7 @@ class TestTask < Test::Unit::TestCase
 
   def test_tasks_can_be_traced
     Rake.application.options.trace = true
-    t1 = intern(:t1) { |t| runlist << t.name; 3321 }
+    t1 = task(:t1)
     out = capture_stdout {
       t1.invoke
     }
@@ -93,9 +83,9 @@ class TestTask < Test::Unit::TestCase
 
   def test_no_double_invoke
     runlist = []
-    t1 = intern(:t1).enhance([:t2, :t3]) { |t| runlist << t.name; 3321 }
-    t2 = intern(:t2).enhance([:t3]) { |t| runlist << t.name }
-    t3 = intern(:t3).enhance { |t| runlist << t.name }
+    t1 = task(:t1 => [:t2, :t3]) { |t| runlist << t.name; 3321 }
+    t2 = task(:t2 => [:t3]) { |t| runlist << t.name }
+    t3 = task(:t3) { |t| runlist << t.name }
     t1.invoke
     assert_equal ["t3", "t2", "t1"], runlist
   end
@@ -155,9 +145,9 @@ class TestTask < Test::Unit::TestCase
   end
 
   def test_investigation_output
-    t1 = intern(:t1).enhance([:t2, :t3]) { |t| runlist << t.name; 3321 }
-    intern(:t2)
-    intern(:t3)
+    t1 = task(:t1 => [:t2, :t3]) { |t| runlist << t.name; 3321 }
+    task(:t2)
+    task(:t3)
     out = t1.investigation
     assert_match(/class:\s*Rake::Task/, out)
     assert_match(/needed:\s*true/, out)
@@ -173,7 +163,7 @@ class TestTask < Test::Unit::TestCase
       name -- Name of task to execute.
       rev  -- Software revision to use.
     }
-    t = intern(:t, :name, :rev)
+    t = task(:t, :name, :rev)
     assert_equal "[name,rev]", t.arg_description
     assert_equal "This is a comment.", t.comment
     assert_match(/^\s*name -- Name/, t.full_comment)
@@ -183,14 +173,14 @@ class TestTask < Test::Unit::TestCase
 
   def test_multiple_comments
     desc "line one"
-    t = intern(:t)
+    t = task(:t)
     desc "line two"
-    intern(:t)
+    task(:t)
     assert_equal "line one / line two", t.comment
   end
 
   def test_settable_comments
-    t = intern(:t)
+    t = task(:t)
     t.comment = "HI"
     assert_equal "HI", t.comment
   end
@@ -200,7 +190,6 @@ end
 class TestTaskWithArguments < Test::Unit::TestCase
   include CaptureStdout
   include Rake
-  include Interning
 
   def setup
     Task.clear
@@ -244,9 +233,7 @@ class TestTaskWithArguments < Test::Unit::TestCase
   end
 
   def test_arg_list_is_empty_if_no_args_given
-    t = intern(:t).enhance do |tt, args|
-      assert_equal({}, args.to_hash)
-    end
+    t = task(:t) { |tt, args| assert_equal({}, args.to_hash) }
     t.invoke(1, 2, 3)
   end
 
@@ -265,7 +252,7 @@ class TestTaskWithArguments < Test::Unit::TestCase
 
   def test_actions_of_various_arity_are_ok_with_args
     notes = []
-    t = intern(:t, :x).enhance do
+    t = task(:t, :x) do
       notes << :a
     end
     t.enhance do | |
@@ -285,14 +272,14 @@ class TestTaskWithArguments < Test::Unit::TestCase
   end
 
   def test_arguments_are_passed_to_block
-    t = intern(:t, :a, :b).enhance { |tt, args|
+    t = task(:t, :a, :b) { |tt, args|
       assert_equal( { :a => 1, :b => 2 }, args.to_hash )
     }
     t.invoke(1, 2)
   end
 
   def test_extra_parameters_are_ignored
-    t = intern(:t, :a).enhance { |tt, args|
+    t = task(:t, :a) { |tt, args|
       assert_equal 1, args.a
       assert_nil args[2]
     }
@@ -315,13 +302,13 @@ class TestTaskWithArguments < Test::Unit::TestCase
   end
 
   def test_block_with_no_parameters_is_ok
-    t = intern(:t).enhance { }
+    t = task(:t) { }
     t.invoke(1, 2)
   end
 
   def test_name_with_args
     desc "T"
-    t = intern(:tt, :a, :b)
+    t = task(:tt, :a, :b)
     assert_equal "tt", t.name
     assert_equal "T", t.comment
     assert_equal "[a,b]", t.arg_description
@@ -331,26 +318,26 @@ class TestTaskWithArguments < Test::Unit::TestCase
 
   def test_named_args_are_passed_to_prereqs
     value = nil
-    pre = intern(:pre, :rev).enhance { |t, args| value = args.rev }
-    t = intern(:t, :name, :rev).enhance([:pre])
+    pre = task(:pre, :rev) { |t, args| value = args.rev }
+    t = task(:t, :name, :rev, :needs => [:pre])
     t.invoke("bill", "1.2")
     assert_equal "1.2", value
   end
 
   def test_args_not_passed_if_no_prereq_names
-    pre = intern(:pre).enhance { |t, args|
+    pre = task(:pre) { |t, args|
       assert_equal({}, args.to_hash)
       assert_equal "bill", args.name
     }
-    t = intern(:t, :name, :rev).enhance([:pre])
+    t = task(:t, :name, :rev, :needs => [:pre])
     t.invoke("bill", "1.2")
   end
 
   def test_args_not_passed_if_no_arg_names
-    pre = intern(:pre, :rev).enhance { |t, args|
+    pre = task(:pre, :rev) { |t, args|
       assert_equal({ :rev => nil }, args.to_hash)
     }
-    t = intern(:t).enhance([:pre])
+    t = task(:t, :needs => [:pre])
     t.invoke("bill", "1.2")
   end
 end
