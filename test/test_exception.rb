9287c00e3afd12d01c722623c55ab4563c8435e3
quix <quixoticsycophant@gmail.com>
2008-09-06 22:33:35 -0400

    cleanup for test_exception

--- test/test_exception.rb
+++ test/test_exception.rb
@@ -1,31 +1,29 @@
 
-here = File.dirname(__FILE__)
-$LOAD_PATH.unshift "#{here}/../lib"
-$LOAD_PATH.unshift "#{here}/../contrib/quix/lib"
-
-require 'comp_tree'
-require 'quix/config'
-require 'test/unit'
-
 #
 # Mean workaround using separate processes due to assert_raise causing
 # problems with threads.
 #
 
+require 'test/unit'
+
+module WorkaroundConfig
+  HERE = File.dirname(__FILE__)
+  LIB_DIR = File.expand_path("#{HERE}/../lib")
+  QUIX_LIB_DIR = File.expand_path("#{HERE}/../contrib/quix/lib")
+  OUTPUT_FILE = "#{HERE}/#{File.basename(__FILE__)}.output"
+  
+  $LOAD_PATH.unshift LIB_DIR
+  $LOAD_PATH.unshift QUIX_LIB_DIR
+end
+  
+require 'comp_tree'
+require 'quix/config'
+
 module CompTree
   class TestRaises < Test::Unit::TestCase
-    HERE = File.dirname(__FILE__)
-    LIB_DIR = File.expand_path("#{HERE}/../lib")
-    QUIX_LIB_DIR = File.expand_path("#{HERE}/../contrib/quix/lib")
+    include WorkaroundConfig
 
-    OUTPUT_FILE = "#{HERE}/#{File.basename(__FILE__)}.output"
-    
-    $LOAD_PATH.unshift LIB_DIR
-    $LOAD_PATH.unshift QUIX_LIB_DIR
-    
-    require 'quix/config'
-
-    def test_1
+    def test_exception
       if RUBY_PLATFORM =~ %r!java!
         puts "skipping #{File.basename(__FILE__)}."
       else
