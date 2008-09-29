16f1395718c6573ff3763e70c04f234a5899a2f2
quix <quixoticsycophant@gmail.com>
2008-09-28 22:06:54 -0400

    clean up parallel output with mutex

--- test/test_parallel.rb
+++ test/test_parallel.rb
@@ -2,28 +2,17 @@
 require 'rbconfig'
 require 'test/unit'
 
-ENV["PATH"] =
-  File.expand_path(File.dirname(__FILE__) + "/../bin") +
-  ":" +
-  ENV["PATH"]
-
 if Rake.application.num_threads > 1
   class TestSimpleParallel < Test::Unit::TestCase
     def test_1
       here = File.dirname(__FILE__)
-      
-      rake =
-        if Config::CONFIG["arch"] =~ %r!java!i
-          "jrake"
-        else
-          "rake"
-        end
-      
+      rake = File.expand_path("#{here}/../bin/rake")
+
       ENV["RUBYLIB"] = lambda {
         lib = File.expand_path("#{here}/../lib")
         current = ENV["RUBYLIB"]
         if current
-          "#{current}:#{lib}"
+          "#{lib}:#{current}"
         else
           lib
         end
@@ -36,7 +25,7 @@ if Rake.application.num_threads > 1
         ].each { |file|
           (1..5).each { |n|
             args = [rake, "--threads", n.to_s, "-f", file]
-            puts("-"*40)
+            puts("\n" + "-"*40)
             puts(args.join(" "))
             assert(system(*args))
           }
