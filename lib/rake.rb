2f6d8059d025203ee0e45fc775376f0a7e8781eb
Jim Weirich <jim.weirich@gmail.com>
2008-12-13 21:36:08 -0500

    updated copyright notices for 2008

--- lib/rake.rb
+++ lib/rake.rb
@@ -2,7 +2,7 @@
 
 #--
 
-# Copyright (c) 2003, 2004, 2005, 2006, 2007  Jim Weirich
+# Copyright 2003, 2004, 2005, 2006, 2007, 2008 by Jim Weirich (jim@weirichhouse.org)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
