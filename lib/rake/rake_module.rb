2de58bc9e1b1ab89564f160c58f768cefa736e70
Jim Weirich <jim.weirich@gmail.com>
2009-03-28 16:49:10 +0000

    Broken into separate files.

---
+++ lib/rake/rake_module.rb
@@ -0,0 +1,23 @@
+module Rake
+
+  # Rake module singleton methods.
+  #
+  class << self
+    # Current Rake Application
+    def application
+      @application ||= Rake::Application.new
+    end
+
+    # Set the current Rake application object.
+    def application=(app)
+      @application = app
+    end
+
+    # Return the original directory where the Rake application was started.
+    def original_dir
+      application.original_dir
+    end
+
+  end
+
+end
