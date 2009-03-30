943c2a90e9cf0c5045cc8cdd43c75edf8deebccf
quix <quixoticsycophant@gmail.com>
2009-03-29 20:39:26 -0400

    fix test_parallel for ruby-1.8.6-mswin

--- test/test_parallel.rb
+++ test/test_parallel.rb
@@ -11,24 +11,23 @@ if Rake.application.num_threads > 1
       ENV["RUBYLIB"] = lambda {
         lib = File.expand_path("#{here}/../lib")
         current = ENV["RUBYLIB"]
+        sep = Rake.application.windows? ? ";" : ":"
         if current
-          "#{lib}:#{current}"
+          "#{lib}#{sep}#{current}"
         else
           lib
         end
       }.call
       
-      Dir.chdir(here) {
-        [
-         "Rakefile.simple",
-         "Rakefile.seq",
-        ].each { |file|
-          (1..5).each { |n|
-            args = [rake, "--threads", n.to_s, "-f", file]
-            puts("\n" + "-"*40)
-            puts(args.join(" "))
-            assert(system(*args))
-          }
+      [
+       "Rakefile.simple",
+       "Rakefile.seq",
+      ].each { |file|
+        (1..5).each { |n|
+          args = [rake, "--threads", n.to_s, "-f", "test/#{file}"]
+          puts("\n" + "-"*40)
+          puts(args.join(" "))
+          assert(ruby(*args))
         }
       }
     end
