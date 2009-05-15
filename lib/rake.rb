bafe1ee8c7525f9c24c2188e71c7381271fbbf6c
Jim Weirich <jim.weirich@gmail.com>
2009-05-15 12:10:00 -0400

    Bumped version to 0.8.7.

--- lib/rake.rb
+++ lib/rake.rb
@@ -29,7 +29,7 @@
 # as a library via a require statement, but it can be distributed
 # independently as an application.
 
-RAKEVERSION = '0.8.6'
+RAKEVERSION = '0.8.7'
 
 require 'rbconfig'
 require 'fileutils'
