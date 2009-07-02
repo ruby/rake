0712aed6a3331064196d1f430064fc20b1e73ef3
unknown <rwarnick@.(none)>
2009-07-02 10:51:18 -0600

    Avoid command line too long problem when running large numbers of unit tests

--- lib/rake/rake_test_loader.rb
+++ lib/rake/rake_test_loader.rb
@@ -2,4 +2,13 @@
 
 # Load the test files from the command line.
 
-ARGV.each { |f| load f unless f =~ /^-/  }
+ARGV.each do |f| 
+  next if f =~ /^-/
+
+  if f =~ /\*/
+    FileList[f].to_a.each { |f| load f }
+  else
+    load f
+  end
+end
+
