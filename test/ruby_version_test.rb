195e3a268ba6e413475fa1108f6d0f52138c8777
Jim Weirich <jim.weirich@gmail.com>
2009-12-22 10:36:13 -0500

    Removed debugging output from the ruby test.

--- test/ruby_version_test.rb
+++ test/ruby_version_test.rb
@@ -2,8 +2,6 @@ require 'test/unit'
 
 class RubyVersionTest < Test::Unit::TestCase
   def test_ruby_version
-    puts(`which ruby`)
-    puts(`which ruby19`)
     puts "\nRUBY VERSION = #{RUBY_VERSION}"
   end
 end
