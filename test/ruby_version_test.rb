ca5a5edfd7a3cfb870c7a043d7491fe9f59d185a
Jim Weirich <jim.weirich@gmail.com>
2009-12-23 09:47:56 -0500

    Moved version display to file level and removed dummy test.

--- test/ruby_version_test.rb
+++ test/ruby_version_test.rb
@@ -1,7 +1,3 @@
 require 'test/unit'
 
-class RubyVersionTest < Test::Unit::TestCase
-  def test_ruby_version
-    puts "\nRUBY VERSION = #{RUBY_VERSION}"
-  end
-end
+puts "\nRUBY VERSION = #{RUBY_VERSION}"
