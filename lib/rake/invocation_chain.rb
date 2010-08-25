b833b6feece39148756fe6a766fffd20751d230a
quix <quixoticsycophant@gmail.com>
2010-08-24 22:44:37 -0400

    InvocationChain#value reader no longer needed; back to mainline rake version

--- lib/rake/invocation_chain.rb
+++ lib/rake/invocation_chain.rb
@@ -4,8 +4,6 @@ module Rake
   # InvocationChain tracks the chain of task invocations to detect
   # circular dependencies.
   class InvocationChain
-    attr_reader :value # :nodoc:
-
     def initialize(value, tail)
       @value = value
       @tail = tail
