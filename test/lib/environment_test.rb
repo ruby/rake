3b5f4b096227887e53e9e9fe6ebc4b77daa051cf
Jim Weirich <jim.weirich@gmail.com>
2011-03-05 15:25:37 -0500

    Improved tests for environment

--- test/lib/environment_test.rb
+++ test/lib/environment_test.rb
@@ -4,15 +4,27 @@ require 'test/test_helper'
 require 'rake/environment'
 
 class TestEnvironment < Test::Unit::TestCase
-  def test_load_string
-    Rake::Task.clear
-    Rake::Environment.load_string("task :xyz")
-    assert Rake::Task[:xyz], "should have a task named xyz"
-  end
-
   def test_load_rakefile
     Rake::Task.clear
     Rake::Environment.load_rakefile("test/data/default/Rakefile")
+    assert Rake::Task[:default], "Should have a default task"
+  end
+
+  def test_run
+    Rake::Task.clear
+    Rake::Environment.run do
+      desc "demo comment"
+      task :default
+    end
+    assert Rake::Task[:default], "Should have a default task"
+  end
+
+  def test_environment
+    Rake::Task.clear
+    Rake::DSL.environment do
+      desc "demo comment"
+      task :default
+    end
     assert Rake::Task[:default], "Should have a default task"
   end
 end
