26636b7c6eb254de63c18e1322253b9a65b88a0b
Eric Hodel <drbrain@segment7.net>
2011-06-22 16:31:28 -0700

    Remove in_environment from test_rake_application_options.rb

--- test/test_rake_application_options.rb
+++ test/test_rake_application_options.rb
@@ -28,312 +28,258 @@ class TestRakeApplicationOptions < Rake::TestCase
   end
 
   def test_default_options
-    in_environment("RAKEOPT" => nil) do
-      opts = command_line
-      assert_nil opts.classic_namespace
-      assert_nil opts.dryrun
-      assert_nil opts.ignore_system
-      assert_nil opts.load_system
-      assert_nil opts.nosearch
-      assert_equal ['rakelib'], opts.rakelib
-      assert_nil opts.show_prereqs
-      assert_nil opts.show_task_pattern
-      assert_nil opts.show_tasks
-      assert_nil opts.silent
-      assert_nil opts.trace
-      assert_equal ['rakelib'], opts.rakelib
-      assert ! Rake::FileUtilsExt.verbose_flag
-      assert ! Rake::FileUtilsExt.nowrite_flag
-    end
+    opts = command_line
+    assert_nil opts.classic_namespace
+    assert_nil opts.dryrun
+    assert_nil opts.ignore_system
+    assert_nil opts.load_system
+    assert_nil opts.nosearch
+    assert_equal ['rakelib'], opts.rakelib
+    assert_nil opts.show_prereqs
+    assert_nil opts.show_task_pattern
+    assert_nil opts.show_tasks
+    assert_nil opts.silent
+    assert_nil opts.trace
+    assert_equal ['rakelib'], opts.rakelib
+    assert ! Rake::FileUtilsExt.verbose_flag
+    assert ! Rake::FileUtilsExt.nowrite_flag
   end
 
   def test_dry_run
-    in_environment do
-      flags('--dry-run', '-n') do |opts|
-        assert opts.dryrun
-        assert opts.trace
-        assert Rake::FileUtilsExt.verbose_flag
-        assert Rake::FileUtilsExt.nowrite_flag
-      end
+    flags('--dry-run', '-n') do |opts|
+      assert opts.dryrun
+      assert opts.trace
+      assert Rake::FileUtilsExt.verbose_flag
+      assert Rake::FileUtilsExt.nowrite_flag
     end
   end
 
   def test_describe
-    in_environment do
-      flags('--describe') do |opts|
-        assert_equal :describe, opts.show_tasks
-        assert_equal(//.to_s, opts.show_task_pattern.to_s)
-      end
+    flags('--describe') do |opts|
+      assert_equal :describe, opts.show_tasks
+      assert_equal(//.to_s, opts.show_task_pattern.to_s)
     end
   end
 
   def test_describe_with_pattern
-    in_environment do
-      flags('--describe=X') do |opts|
-        assert_equal :describe, opts.show_tasks
-        assert_equal(/X/.to_s, opts.show_task_pattern.to_s)
-      end
+    flags('--describe=X') do |opts|
+      assert_equal :describe, opts.show_tasks
+      assert_equal(/X/.to_s, opts.show_task_pattern.to_s)
     end
   end
 
   def test_execute
-    in_environment do
+    $xyzzy = 0
+    flags('--execute=$xyzzy=1', '-e $xyzzy=1') do |opts|
+      assert_equal 1, $xyzzy
+      assert_equal :exit, @exit
       $xyzzy = 0
-      flags('--execute=$xyzzy=1', '-e $xyzzy=1') do |opts|
-        assert_equal 1, $xyzzy
-        assert_equal :exit, @exit
-        $xyzzy = 0
-      end
     end
   end
 
   def test_execute_and_continue
-    in_environment do
+    $xyzzy = 0
+    flags('--execute-continue=$xyzzy=1', '-E $xyzzy=1') do |opts|
+      assert_equal 1, $xyzzy
+      refute_equal :exit, @exit
       $xyzzy = 0
-      flags('--execute-continue=$xyzzy=1', '-E $xyzzy=1') do |opts|
-        assert_equal 1, $xyzzy
-        refute_equal :exit, @exit
-        $xyzzy = 0
-      end
     end
   end
 
   def test_execute_and_print
-    in_environment do
-      $xyzzy = 0
-      out, = capture_io do
-        flags('--execute-print=$xyzzy="pugh"', '-p $xyzzy="pugh"') do |opts|
-          assert_equal 'pugh', $xyzzy
-          assert_equal :exit, @exit
-          $xyzzy = 0
-        end
+    $xyzzy = 0
+    out, = capture_io do
+      flags('--execute-print=$xyzzy="pugh"', '-p $xyzzy="pugh"') do |opts|
+        assert_equal 'pugh', $xyzzy
+        assert_equal :exit, @exit
+        $xyzzy = 0
       end
-
-      assert_match(/^pugh$/, out)
     end
+
+    assert_match(/^pugh$/, out)
   end
 
   def test_help
-    in_environment do
-      out, = capture_io do
-        flags '--help', '-H', '-h'
-      end
-
-      assert_match(/\Arake/, out)
-      assert_match(/\boptions\b/, out)
-      assert_match(/\btargets\b/, out)
-      assert_equal :exit, @exit
+    out, = capture_io do
+      flags '--help', '-H', '-h'
     end
+
+    assert_match(/\Arake/, out)
+    assert_match(/\boptions\b/, out)
+    assert_match(/\btargets\b/, out)
+    assert_equal :exit, @exit
   end
 
   def test_libdir
-    in_environment do
-      flags(['--libdir', 'xx'], ['-I', 'xx'], ['-Ixx']) do |opts|
-        $:.include?('xx')
-      end
+    flags(['--libdir', 'xx'], ['-I', 'xx'], ['-Ixx']) do |opts|
+      $:.include?('xx')
     end
   ensure
     $:.delete('xx')
   end
 
   def test_rakefile
-    in_environment do
-      flags(['--rakefile', 'RF'], ['--rakefile=RF'], ['-f', 'RF'], ['-fRF']) do |opts|
-        assert_equal ['RF'], @app.instance_eval { @rakefiles }
-      end
+    flags(['--rakefile', 'RF'], ['--rakefile=RF'], ['-f', 'RF'], ['-fRF']) do |opts|
+      assert_equal ['RF'], @app.instance_eval { @rakefiles }
     end
   end
 
   def test_rakelib
-    in_environment do
-      flags(['--rakelibdir', 'A:B:C'], ['--rakelibdir=A:B:C'], ['-R', 'A:B:C'], ['-RA:B:C']) do |opts|
-        assert_equal ['A', 'B', 'C'], opts.rakelib
-      end
+    flags(['--rakelibdir', 'A:B:C'], ['--rakelibdir=A:B:C'], ['-R', 'A:B:C'], ['-RA:B:C']) do |opts|
+      assert_equal ['A', 'B', 'C'], opts.rakelib
     end
   end
 
   def test_require
-    in_environment do
-      flags(['--require', 'test/reqfile'], '-rtest/reqfile2', '-rtest/reqfile3') do |opts|
-      end
-      assert TESTING_REQUIRE.include?(1)
-      assert TESTING_REQUIRE.include?(2)
-      assert TESTING_REQUIRE.include?(3)
-      assert_equal 3, TESTING_REQUIRE.size
+    flags(['--require', 'test/reqfile'], '-rtest/reqfile2', '-rtest/reqfile3') do |opts|
     end
+    assert TESTING_REQUIRE.include?(1)
+    assert TESTING_REQUIRE.include?(2)
+    assert TESTING_REQUIRE.include?(3)
+    assert_equal 3, TESTING_REQUIRE.size
   end
 
   def test_missing_require
-    in_environment do
-      ex = assert_raises(LoadError) do
-        flags(['--require', 'test/missing']) do |opts|
-        end
+    ex = assert_raises(LoadError) do
+      flags(['--require', 'test/missing']) do |opts|
       end
-      assert_match(/such file/, ex.message)
-      assert_match(/test\/missing/, ex.message)
     end
+    assert_match(/such file/, ex.message)
+    assert_match(/test\/missing/, ex.message)
   end
 
   def test_prereqs
-    in_environment do
-      flags('--prereqs', '-P') do |opts|
-        assert opts.show_prereqs
-      end
+    flags('--prereqs', '-P') do |opts|
+      assert opts.show_prereqs
     end
   end
 
   def test_quiet
-    in_environment do
-      flags('--quiet', '-q') do |opts|
-        assert ! Rake::FileUtilsExt.verbose_flag
-        assert ! opts.silent
-      end
+    flags('--quiet', '-q') do |opts|
+      assert ! Rake::FileUtilsExt.verbose_flag
+      assert ! opts.silent
     end
   end
 
   def test_no_search
-    in_environment do
-      flags('--nosearch', '--no-search', '-N') do |opts|
-        assert opts.nosearch
-      end
+    flags('--nosearch', '--no-search', '-N') do |opts|
+      assert opts.nosearch
     end
   end
 
   def test_silent
-    in_environment do
-      flags('--silent', '-s') do |opts|
-        assert ! Rake::FileUtilsExt.verbose_flag
-        assert opts.silent
-      end
+    flags('--silent', '-s') do |opts|
+      assert ! Rake::FileUtilsExt.verbose_flag
+      assert opts.silent
     end
   end
 
   def test_system
-    in_environment do
-      flags('--system', '-g') do |opts|
-        assert opts.load_system
-      end
+    flags('--system', '-g') do |opts|
+      assert opts.load_system
     end
   end
 
   def test_no_system
-    in_environment do
-      flags('--no-system', '-G') do |opts|
-        assert opts.ignore_system
-      end
+    flags('--no-system', '-G') do |opts|
+      assert opts.ignore_system
     end
   end
 
   def test_trace
-    in_environment do
-      flags('--trace', '-t') do |opts|
-        assert opts.trace
-        assert Rake::FileUtilsExt.verbose_flag
-        assert ! Rake::FileUtilsExt.nowrite_flag
-      end
+    flags('--trace', '-t') do |opts|
+      assert opts.trace
+      assert Rake::FileUtilsExt.verbose_flag
+      assert ! Rake::FileUtilsExt.nowrite_flag
     end
   end
 
   def test_trace_rules
-    in_environment do
-      flags('--rules') do |opts|
-        assert opts.trace_rules
-      end
+    flags('--rules') do |opts|
+      assert opts.trace_rules
     end
   end
 
   def test_tasks
-    in_environment do
-      flags('--tasks', '-T') do |opts|
-        assert_equal :tasks, opts.show_tasks
-        assert_equal(//.to_s, opts.show_task_pattern.to_s)
-      end
-      flags(['--tasks', 'xyz'], ['-Txyz']) do |opts|
-        assert_equal :tasks, opts.show_tasks
-        assert_equal(/xyz/.to_s, opts.show_task_pattern.to_s)
-      end
+    flags('--tasks', '-T') do |opts|
+      assert_equal :tasks, opts.show_tasks
+      assert_equal(//.to_s, opts.show_task_pattern.to_s)
+    end
+    flags(['--tasks', 'xyz'], ['-Txyz']) do |opts|
+      assert_equal :tasks, opts.show_tasks
+      assert_equal(/xyz/.to_s, opts.show_task_pattern.to_s)
     end
   end
 
   def test_where
-    in_environment do
-      flags('--where', '-W') do |opts|
-        assert_equal :lines, opts.show_tasks
-        assert_equal(//.to_s, opts.show_task_pattern.to_s)
-      end
-      flags(['--where', 'xyz'], ['-Wxyz']) do |opts|
-        assert_equal :lines, opts.show_tasks
-        assert_equal(/xyz/.to_s, opts.show_task_pattern.to_s)
-      end
+    flags('--where', '-W') do |opts|
+      assert_equal :lines, opts.show_tasks
+      assert_equal(//.to_s, opts.show_task_pattern.to_s)
+    end
+    flags(['--where', 'xyz'], ['-Wxyz']) do |opts|
+      assert_equal :lines, opts.show_tasks
+      assert_equal(/xyz/.to_s, opts.show_task_pattern.to_s)
     end
   end
 
   def test_no_deprecated_messages
-    in_environment do
-      flags('--no-deprecation-warnings', '-X') do |opts|
-        assert opts.ignore_deprecate
-      end
+    flags('--no-deprecation-warnings', '-X') do |opts|
+      assert opts.ignore_deprecate
     end
   end
 
   def test_verbose
-    in_environment do
-      out, = capture_io do
-        flags('--verbose', '-V') do |opts|
-          assert Rake::FileUtilsExt.verbose_flag
-          assert ! opts.silent
-        end
+    out, = capture_io do
+      flags('--verbose', '-V') do |opts|
+        assert Rake::FileUtilsExt.verbose_flag
+        assert ! opts.silent
       end
-
-      assert_equal "rake, version #{Rake::VERSION}\n", out
     end
+
+    assert_equal "rake, version #{Rake::VERSION}\n", out
   end
 
   def test_version
-    in_environment do
-      out, = capture_io do
-        flags '--version', '-V'
-      end
-
-      assert_match(/\bversion\b/, out)
-      assert_match(/\b#{RAKEVERSION}\b/, out)
-      assert_equal :exit, @exit
+    out, = capture_io do
+      flags '--version', '-V'
     end
+
+    assert_match(/\bversion\b/, out)
+    assert_match(/\b#{RAKEVERSION}\b/, out)
+    assert_equal :exit, @exit
   end
 
   def test_classic_namespace
-    in_environment do
-      _, err = capture_io do
-        flags(['--classic-namespace'],
-              ['-C', '-T', '-P', '-n', '-s', '-t']) do |opts|
-          assert opts.classic_namespace
-          assert_equal opts.show_tasks, $show_tasks
-          assert_equal opts.show_prereqs, $show_prereqs
-          assert_equal opts.trace, $trace
-          assert_equal opts.dryrun, $dryrun
-          assert_equal opts.silent, $silent
-        end
-      end
-
-      assert_match(/deprecated/, err)
+    _, err = capture_io do
+      flags(['--classic-namespace'],
+            ['-C', '-T', '-P', '-n', '-s', '-t']) do |opts|
+        assert opts.classic_namespace
+        assert_equal opts.show_tasks, $show_tasks
+        assert_equal opts.show_prereqs, $show_prereqs
+        assert_equal opts.trace, $trace
+        assert_equal opts.dryrun, $dryrun
+        assert_equal opts.silent, $silent
+            end
     end
+
+    assert_match(/deprecated/, err)
   end
 
   def test_bad_option
-    in_environment do
-      _, err = capture_io do
-        ex = assert_raises(OptionParser::InvalidOption) do
-          flags('--bad-option')
-        end
-        if ex.message =~ /^While/ # Ruby 1.9 error message
-          assert_match(/while parsing/i, ex.message)
-        else                      # Ruby 1.8 error message
-          assert_match(/(invalid|unrecognized) option/i, ex.message)
-          assert_match(/--bad-option/, ex.message)
-        end
+    _, err = capture_io do
+      ex = assert_raises(OptionParser::InvalidOption) do
+        flags('--bad-option')
       end
-      assert_equal '', err
+
+      if ex.message =~ /^While/ # Ruby 1.9 error message
+        assert_match(/while parsing/i, ex.message)
+      else                      # Ruby 1.8 error message
+        assert_match(/(invalid|unrecognized) option/i, ex.message)
+        assert_match(/--bad-option/, ex.message)
+      end
     end
+
+    assert_equal '', err
   end
 
   def test_task_collection
