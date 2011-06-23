3fbd6777f312decb56501c1cdcd372f653e9eb8f
Eric Hodel <drbrain@segment7.net>
2011-06-23 16:02:49 -0700

    Sort tests in test_rake_test_task

--- test/test_rake_test_task.rb
+++ test/test_rake_test_task.rb
@@ -28,13 +28,6 @@ class TestRakeTestTask < Rake::TestCase
     assert Task.task_defined?(:example)
   end
 
-  def test_pattern_equals
-    tt = Rake::TestTask.new do |t|
-      t.pattern = '*.rb'
-    end
-    assert_equal ['*.rb'], tt.file_list.to_a
-  end
-
   def test_file_list_ENV_TEST
     ENV['TEST'] = 'testfile.rb'
     tt = Rake::TestTask.new do |t|
@@ -46,12 +39,29 @@ class TestRakeTestTask < Rake::TestCase
     ENV.delete 'TEST'
   end
 
-  def test_test_files_equals
-    tt = Rake::TestTask.new do |t|
-      t.test_files = FileList['a.rb', 'b.rb']
+  def test_libs_equals
+    test_task = Rake::TestTask.new do |t|
+      t.libs << ["A", "B"]
     end
 
-    assert_equal ["a.rb", 'b.rb'], tt.file_list.to_a
+    path = %w[lib A B].join File::PATH_SEPARATOR
+
+    assert_equal "-I\"#{path}\"", test_task.ruby_opts_string
+  end
+
+  def test_libs_equals_empty
+    test_task = Rake::TestTask.new do |t|
+      t.libs = []
+    end
+
+    assert_equal '', test_task.ruby_opts_string
+  end
+
+  def test_pattern_equals
+    tt = Rake::TestTask.new do |t|
+      t.pattern = '*.rb'
+    end
+    assert_equal ['*.rb'], tt.file_list.to_a
   end
 
   def test_pattern_equals_test_files_equals
@@ -68,6 +78,14 @@ class TestRakeTestTask < Rake::TestCase
     end
 
     assert_equal '-e "ARGV.each{|f| require f}"', test_task.run_code
+  end
+
+  def test_run_code_rake
+    test_task = Rake::TestTask.new do |t|
+      t.loader = :rake
+    end
+
+    assert_match(/-I".*?" ".*?"/, test_task.run_code)
   end
 
   def test_run_code_testrb_ruby_1_8_2
@@ -90,30 +108,12 @@ class TestRakeTestTask < Rake::TestCase
     assert_match(/^-S testrb +$/, test_task.run_code)
   end
 
-  def test_run_code_rake
-    test_task = Rake::TestTask.new do |t|
-      t.loader = :rake
+  def test_test_files_equals
+    tt = Rake::TestTask.new do |t|
+      t.test_files = FileList['a.rb', 'b.rb']
     end
 
-    assert_match(/-I".*?" ".*?"/, test_task.run_code)
-  end
-
-  def test_libs_equals
-    test_task = Rake::TestTask.new do |t|
-      t.libs << ["A", "B"]
-    end
-
-    path = %w[lib A B].join File::PATH_SEPARATOR
-
-    assert_equal "-I\"#{path}\"", test_task.ruby_opts_string
-  end
-
-  def test_libs_equals_empty
-    test_task = Rake::TestTask.new do |t|
-      t.libs = []
-    end
-
-    assert_equal '', test_task.ruby_opts_string
+    assert_equal ["a.rb", 'b.rb'], tt.file_list.to_a
   end
 
 end
