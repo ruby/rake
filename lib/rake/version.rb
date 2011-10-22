ffb760195a7ee22d7363583585c94421a38abc9a
Jim Weirich <jim.weirich@gmail.com>
2011-10-22 11:10:25 -0400

    Bump to version 0.9.2.2

--- lib/rake/version.rb
+++ lib/rake/version.rb
@@ -1,8 +1,8 @@
 module Rake
-  VERSION = '0.9.2'
+  VERSION = '0.9.2.2'
 
   module Version # :nodoc: all
-    MAJOR, MINOR, BUILD = VERSION.split '.'
-    NUMBERS = [ MAJOR, MINOR, BUILD ]
+    MAJOR, MINOR, BUILD, PATCH = VERSION.split('.')
+    NUMBERS = [ MAJOR, MINOR, BUILD, PATCH ]
   end
 end
