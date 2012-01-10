e01bb8fc398cb0bbc9314af177db469c414468d2
Ryan Davis <ryand-ruby@zenspider.com>
2012-01-09 17:14:21 -0800

    + Added global :phony task with an epoch timestamp to help with unruly dependencies

--- lib/rake.rb
+++ lib/rake.rb
@@ -68,3 +68,14 @@ $trace = false
 
 FileList = Rake::FileList
 RakeFileUtils = Rake::FileUtilsExt
+
+##
+# Defines a :phony task that you can use as a dependency. This allows
+# file-based tasks to use non-file-based tasks as prerequisites
+# without forcing them to rebuild.
+#
+# See FileTask#out_of_date? and Task#timestamp for more info.
+
+def (Rake::Task.define_task(:phony)).timestamp
+  Time.at 0
+end
