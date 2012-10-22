3a797bf569c9f0ba84359dc40851f059e470300f
Jim Weirich <jim.weirich@gmail.com>
2012-10-22 14:32:25 -0400

    whitespace removed

--- test/test_rake_thread_pool.rb
+++ test/test_rake_thread_pool.rb
@@ -11,18 +11,18 @@ class TestRakeTestThreadPool < Rake::TestCase
     pool.join
     assert_equal Thread.current, f.call
   end
-  
+
   def test_pool_executes_in_other_thread_for_pool_of_size_one
     pool = ThreadPool.new(1)
     f = pool.future{Thread.current}
     pool.join
     refute_equal Thread.current, f.call
   end
-  
+
   def test_pool_executes_in_two_other_threads_for_pool_of_size_two
     pool = ThreadPool.new(2)
     threads = 2.times.collect{ pool.future{ sleep 0.1; Thread.current } }.each{|f|f.call}
-    
+
     refute_equal threads[0], threads[1]
     refute_equal Thread.current, threads[0]
     refute_equal Thread.current, threads[1]
@@ -52,7 +52,7 @@ class TestRakeTestThreadPool < Rake::TestCase
     assert_equal 'a', a
     assert_equal 'b'.capitalize, b
   end
-  
+
   def test_pool_join_empties_queue
     pool = ThreadPool.new(2)
     repeat = 25
@@ -85,7 +85,7 @@ class TestRakeTestThreadPool < Rake::TestCase
     assert_raises(Exception) do
       pool.future(2, &deep_exception_block).call
     end
-    
+
   end
 
   def test_pool_always_has_max_threads_doing_work
@@ -99,21 +99,21 @@ class TestRakeTestThreadPool < Rake::TestCase
     future3 = pool.future { sleep 0.01 }
 
     sleep initial_sleep_time / 2.0 # wait for everything to queue up
-    
+
     # at this point, we should have 5 threads sleeping depending on future1, and
     # two threads doing work on future1 and future 2.
     assert_equal pool.__send__(:__threads__).count, 7
-    
+
     # future 3 is in the queue because there aren't enough active threads to work on it.
     assert_equal pool.__send__(:__queue__).size, 1
-    
+
     [future1, dependent_futures, future2, future3].flatten.each { |f| f.call }
     pool.join
   end
 
   def test_pool_prevents_deadlock
     pool = ThreadPool.new(5)
-    
+
     common_dependency_a = pool.future { sleep 0.2 }
     futures_a = 10.times.collect { pool.future{ common_dependency_a.call; sleep(rand() * 0.01) } }
 
@@ -126,11 +126,11 @@ class TestRakeTestThreadPool < Rake::TestCase
 
   def test_pool_reports_correct_results
     pool = ThreadPool.new(7)
-    
+
     a = 18
     b = 5
     c = 3
-    
+
     result = a.times.collect do
       pool.future do
         b.times.collect do
@@ -138,10 +138,9 @@ class TestRakeTestThreadPool < Rake::TestCase
         end.inject(0) { |m,f| m+f.call }
       end
     end.inject(0) { |m,f| m+f.call }
-    
+
     assert_equal( (a*b*c), result )
     pool.join
   end
 
 end
-
