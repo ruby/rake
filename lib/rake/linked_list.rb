c9886cfff5eed87339485c8dd55efe45929d94d7
Jim Weirich <jim.weirich@gmail.com>
2013-04-30 14:19:57 +0000

    Added linked list class.

---
+++ lib/rake/linked_list.rb
@@ -0,0 +1,66 @@
+module Rake
+
+  class LinkedList
+    include Enumerable
+
+    attr_reader :head, :tail
+
+    def initialize(head, tail=EMPTY)
+      @head = head
+      @tail = tail
+    end
+
+    def empty?
+      false
+    end
+
+    def ==(other)
+      current = self
+      while ! current.empty? && ! other.empty?
+        return false if current.head != other.head
+        current = current.tail
+        other = other.tail
+      end
+      current.empty? && other.empty?
+    end
+
+    def to_s
+      items = map { |item| item.to_s }.join(", ")
+      "LL(#{items})"
+    end
+
+    def inspect
+      items = map { |item| item.inspect }.join(", ")
+      "LL(#{items})"
+    end
+
+    def each
+      current = self
+      while ! current.empty?
+        yield(current.head)
+        current = current.tail
+      end
+      self
+    end
+
+    def self.make(*args)
+      result = EMPTY
+      args.reverse_each do |item|
+        result = LinkedList.new(item, result)
+      end
+      result
+    end
+
+    class EmptyLinkedList < LinkedList
+      def initialize
+      end
+
+      def empty?
+        true
+      end
+    end
+
+    EMPTY = EmptyLinkedList.new
+  end
+
+end
