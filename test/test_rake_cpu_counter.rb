a8f90cf8aedeefbd79683037e9567f6088d0eebd
Eric Hodel <drbrain@segment7.net>
2014-03-11 19:49:30 -0700

    Test CpuCounter#run

--- test/test_rake_cpu_counter.rb
+++ test/test_rake_cpu_counter.rb
@@ -26,5 +26,19 @@ class TestRakeCpuCounter < Rake::TestCase
     skip 'cannot find which for this test'
   end
 
+  def test_run
+    ruby     = File.basename Gem.ruby
+    ruby_dir = File.dirname  Gem.ruby
+
+    begin
+      orig_path, ENV['PATH'] =
+        ENV['PATH'], [ruby_dir, *ENV['PATH']].join(File::PATH_SEPARATOR)
+
+      assert_equal 7, @cpu_counter.run(ruby, '-e "puts 3 + 4"')
+    ensure
+      ENV['PATH'] = orig_path
+    end
+  end
+
 end
 
