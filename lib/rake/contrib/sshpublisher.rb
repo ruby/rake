00897eaa797de0631957761e722eac07700b6b11
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2016-01-26 20:23:24 +0900

    Move CompositePublisher to lib/rake/contrib/sshpublisher.rb

--- lib/rake/contrib/sshpublisher.rb
+++ lib/rake/contrib/sshpublisher.rb
@@ -2,6 +2,22 @@ require 'rake/dsl_definition'
 require 'rake/contrib/compositepublisher'
 
 module Rake
+  # Manage several publishers as a single entity.
+  class CompositePublisher # :nodoc:
+    def initialize
+      @publishers = []
+    end
+
+    # Add a publisher to the composite.
+    def add(pub)
+      @publishers << pub
+    end
+
+    # Upload all the individual publishers.
+    def upload
+      @publishers.each { |p| p.upload }
+    end
+  end
 
   # Publish an entire directory to an existing remote directory using
   # SSH.
