f2a38bcfd7f0b993a7e2626100e39db2d57f1999
Brian Henderson <henderson.bj@gmail.com>
2016-09-27 16:00:16 -0700

    Teach cleanup to respect the trace option.

--- test/test_rake_clean.rb
+++ test/test_rake_clean.rb
@@ -32,7 +32,55 @@ class TestRakeClean < Rake::TestCase
     refute_match(/failed to remove/i, out)
   end
 
+  def test_cleanup_trace
+    file_name = create_file
+
+    assert_output "", "rm -r #{file_name}\n" do
+      with_trace true do
+        Rake::Cleaner.cleanup(file_name)
+      end
+    end
+  end
+
+  def test_cleanup_without_trace
+    file_name = create_file
+
+    assert_output "", "" do
+      with_trace false do
+        Rake::Cleaner.cleanup(file_name)
+      end
+    end
+  end
+
+  def test_cleanup_opt_overrides_trace_silent
+    file_name = create_file
+
+    assert_output "", "" do
+      with_trace true do
+        Rake::Cleaner.cleanup(file_name, verbose: false)
+      end
+    end
+  end
+
+  def test_cleanup_opt_overrides_trace_verbose
+    file_name = create_file
+
+    assert_output "", "rm -r #{file_name}\n" do
+      with_trace false do
+        Rake::Cleaner.cleanup(file_name, verbose: true)
+      end
+    end
+  end
+
   private
+
+  def create_file
+    dir_name = File.join(@tempdir, "deletedir")
+    file_name = File.join(dir_name, "deleteme")
+    FileUtils.mkdir(dir_name)
+    FileUtils.touch(file_name)
+    file_name
+  end
 
   def create_undeletable_file
     dir_name = File.join(@tempdir, "deletedir")
@@ -57,5 +105,17 @@ class TestRakeClean < Rake::TestCase
     FileUtils.chmod(0777, file_name)
     Rake::Cleaner.cleanup(file_name, verbose: false)
     Rake::Cleaner.cleanup(dir_name, verbose: false)
+  end
+
+  def with_trace value
+    old, Rake.application.options.trace =
+      Rake.application.options.trace, value
+
+    # FileUtils caches the $stderr object, which breaks capture_io et. al.
+    # We hack it here where it's convenient to do so.
+    Rake::Cleaner.instance_variable_set :@fileutils_output, nil
+    yield
+  ensure
+    Rake.application.options.trace = old
   end
 end
