48a9c463ab534df2229f80e7bc13ad61928d3883
Eric Hodel <drbrain@segment7.net>
2017-02-23 10:44:49 -0800

    Make LoadError from running tests more obvious

--- lib/rake/rake_test_loader.rb
+++ lib/rake/rake_test_loader.rb
@@ -2,19 +2,23 @@ require "rake"
 
 # Load the test files from the command line.
 argv = ARGV.select do |argument|
-  case argument
-  when /^-/ then
-    argument
-  when /\*/ then
-    FileList[argument].to_a.each do |file|
-      require File.expand_path file
+  begin
+    case argument
+    when /^-/ then
+      argument
+    when /\*/ then
+      FileList[argument].to_a.each do |file|
+        require File.expand_path file
+      end
+
+      false
+    else
+      require File.expand_path argument
+
+      false
     end
-
-    false
-  else
-    require File.expand_path argument
-
-    false
+  rescue LoadError => e
+    abort "\n#{e.message}\n\n"
   end
 end
 
