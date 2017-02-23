66ec746a9e85c6ec7e7d2e682b3a5b08914a5033
Eric Hodel <drbrain@segment7.net>
2017-02-23 11:04:11 -0800

    Test LoadError behavior

--- test/test_rake_rake_test_loader.rb
+++ test/test_rake_rake_test_loader.rb
@@ -2,6 +2,12 @@ require File.expand_path("../helper", __FILE__)
 
 class TestRakeRakeTestLoader < Rake::TestCase
 
+  def setup
+    super
+
+    @loader = File.join @rake_lib, 'rake/rake_test_loader.rb'
+  end
+
   def test_pattern
     orig_loaded_features = $:.dup
     FileUtils.touch "foo.rb"
@@ -10,11 +16,26 @@ class TestRakeRakeTestLoader < Rake::TestCase
 
     ARGV.replace %w[foo.rb test_*.rb -v]
 
-    load File.join(@rake_lib, "rake/rake_test_loader.rb")
+    load @loader
 
     assert_equal %w[-v], ARGV
   ensure
     $:.replace orig_loaded_features
   end
 
+  def test_load_error
+    expected = <<-EXPECTED
+
+cannot load such file -- #{File.join @tempdir, 'no_such_test_file.rb'}
+
+    EXPECTED
+
+    assert_output nil, expected do
+      ARGV.replace %w[no_such_test_file.rb]
+
+      assert_raises SystemExit do
+        load @loader
+      end
+    end
+  end
 end
