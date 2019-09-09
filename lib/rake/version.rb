ab835523b29543092e31a81d7d620b7d90b1678c
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-09-09 15:54:11 +0900

    bump version to 13.0.0.pre.1

--- lib/rake/version.rb
+++ lib/rake/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Rake
-  VERSION = "12.3.3"
+  VERSION = "13.0.0.pre.1"
 
   module Version # :nodoc: all
     MAJOR, MINOR, BUILD, *OTHER = Rake::VERSION.split "."
