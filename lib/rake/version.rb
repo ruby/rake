d8aba43cfe7c42b16856c85dcc6ee3e2b9aff01c
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-09-24 21:27:05 +0900

    Prepare to release rake 13

--- lib/rake/version.rb
+++ lib/rake/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Rake
-  VERSION = "13.0.0.pre.1"
+  VERSION = "13.0.0"
 
   module Version # :nodoc: all
     MAJOR, MINOR, BUILD, *OTHER = Rake::VERSION.split "."
